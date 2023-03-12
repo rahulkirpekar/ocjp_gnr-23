1b039876179951e85d7bd1287bdd6aab55fb3b04
rahul kirpekar <rahul.kirpekar@gmail.com>
2023-03-12 19:42:46 +0530

    naming rules

---
+++ basic/Royal.java
@@ -0,0 +1,32 @@
+class A
+{
+	
+}
+class B
+{
+	public static void main(String args[])
+	{
+		System.out.println("B---MAIN METHOD");
+	}
+}
+class C
+{
+	public static void main(String args[])
+	{
+		System.out.println("C---MAIN METHOD");
+	}
+}
+class D
+{
+	public static void main(String args[])
+	{
+		System.out.println("D---MAIN METHOD");
+	}
+}
+class TestApp
+{
+	public static void main(String args[])
+	{
+		System.out.println("TestAPP---MAIN METHOD");
+	}
+}
