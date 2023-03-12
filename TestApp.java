6b99c523743fa72ed9e8dba5fd89d5be363cfa45
rahul kirpekar <rahul.kirpekar@gmail.com>
2023-03-12 19:29:23 +0530

    fist session

---
+++ TestApp.java
@@ -0,0 +1,16 @@
+
+// System,String --- class
+public class TestApp
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+	}
+}
+/*
+1) javac JavaFileName.java
+
+2) java ClassName
+*/
+
+
